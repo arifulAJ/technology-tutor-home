2e62a7cf03e7efcf415576e4c38ab7782d15ba4f
arifulislam10111999 <arifulislam10111999@gmail.com>
2022-03-29 17:34:20 +0600

    all

--- src/pages/Services/ServicTec.js
+++ src/pages/Services/ServicTec.js
@@ -6,7 +6,7 @@ const ServicTec = () => {
             <div class="row row-cols-1 row-cols-md-2 g-4">
   <div class="col">
     <div class="card">
-      <img src="..." class="card-img-top" alt="..."/>
+      <img src="https://media.istockphoto.com/photos/hacker-trying-to-get-access-into-a-network-server-at-a-corporate-picture-id1347972948?k=20&m=1347972948&s=612x612&w=0&h=r9M13SxuX7z9nCEm2yBmz6Mzsus3c0H52Wb7WjipRtU=" class="card-img-top" alt="..."/>
       <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -15,7 +15,7 @@ const ServicTec = () => {
   </div>
   <div class="col">
     <div class="card">
-      <img src="..." class="card-img-top" alt="..."/>
+      <img src="https://media.istockphoto.com/photos/woman-hand-on-a-laptop-keyboard-picture-id668650714?k=20&m=668650714&s=612x612&w=0&h=Ju_jztkUikmQP9IeAtqnwIRfqS10uaZV2WlVFDT1BIs=" class="card-img-top" alt="..."/>
       <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -24,7 +24,7 @@ const ServicTec = () => {
   </div>
   <div class="col">
     <div class="card">
-      <img src="..." class="card-img-top" alt="..."/>
+      <img src="https://media.istockphoto.com/photos/graphic-designer-at-work-color-samples-picture-id628814432?k=20&m=628814432&s=612x612&w=0&h=qQj7aCjbWr-hHRMl0bf0GfAoWMiqh3wmllrowpqsVe8=" class="card-img-top" alt="..."/>
       <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
@@ -33,7 +33,7 @@ const ServicTec = () => {
   </div>
   <div class="col">
     <div class="card">
-      <img src="..." class="card-img-top" alt="..."/>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtQ9CbmvuQsFOP6TPDHTswxRQcxvGBErbEjA&usqp=CAU" class="card-img-top" alt="..."/>
       <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
